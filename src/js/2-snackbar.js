0a8ebea85be4480417d43afa8b50059425143c5f
Vikroria Fartushna <vika.beis123@gmail.com>
2026-03-11 01:29:32 +0200

    fix snackbar notification messages

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -12,13 +12,13 @@ form.addEventListener('submit', event => {
   createPromise(delay, state)
     .then(delay => {
       iziToast.success({
-        message: `Fulfilled promise in ${delay}ms`,
+        message: `✅ Fulfilled promise in ${delay}ms`,
         position: 'topRight',
       });
     })
     .catch(delay => {
       iziToast.error({
-        message: `Rejected promise in ${delay}ms`,
+        message: `❌ Rejected promise in ${delay}ms`,
         position: 'topRight',
       });
     });
